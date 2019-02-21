b2e80ec25248a4d0813a7daea7652c3d4921247a
Gabe Gaerlan <ggaerlan@csumb.edu>
2019-02-21 22:46:22 +0000

    fixed

--- searchPage.php
+++ searchPage.php
@@ -1,3 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="styles.css" media="all and (max-width: 1024px)">
+    <title>Gabe Boba Training</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">Gabes Boba Helper</a>
+      </nav>
+    </header>
+    
 <?php
 mysql_connect("us-cdbr-iron-east-03.cleardb.net","b1f0aa2908c861","0eb70cc4") or die ("could not connect");
 mysql_select_db("heroku_e9d1cc3ad148014") or die("could not find db");
@@ -29,21 +46,6 @@ if(isset($_POST['search'])){
     }
 }
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="styles.css" media="all and (max-width: 1024px)">
-    <title>Gabe Boba Training</title>
-    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head>
-  <body>
-    <header>
-      <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="index.php">Gabes Boba Helper</a>
-      </nav>
-    </header>
     <main class="container">
         <section>
             <p>
